2cf532db8547f96bd5b0d788495fe437aae6af85
MauriRamos19 <68834179+MauriRamos19@users.noreply.github.com>
2022-08-21 19:16:11 -0600

    Changes in Olvin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
 function App() {
 
 	return (
-		<div className="App">
-			<Outlet />
-		</div>
+		<ChakraProvider theme={theme}>
+			<div className="App">
+				<Outlet />
+			</div>
+		</ChakraProvider>
 	);
 }
 
--- src/Pages/JsonView/JsonView.js
+++ src/Pages/JsonView/JsonView.js
@@ -5,7 +5,125 @@ import Header from "../../Components/Header/Header";
 import Button from "../../Components/Button/Button";
 import { Link, useLocation } from "react-router-dom";
 
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
+
 import "./JsonView.css";
+
+const json = [
+    {
+        "anio": "2020",
+        "meses": [
+            {
+                "name": "Enero",
+                "productos": [
+                    {
+                        "id": "1",
+                        "name": "Producto 1",
+                        "precio": "1",
+                        "cantidad": "1"
+                    }
+                ]
+
+            },
+        ]  
+    },
+     {
+        "anio": "2021",
+        "meses": [
+            {
+                "name": "Enero",
+                "productos": [
+                    {
+                        "id": "1",
+                        "name": "Producto 1",
+                        "precio": "1",
+                        "cantidad": "1"
+                    }
+                ]
+
+            },{
+                "name": "Febrero",
+                "productos": [
+                    {
+                        "id": "1",
+                        "name": "Producto 1",
+                        "precio": "1",
+                        "cantidad": "1"
+                    }
+                ]
+
+            }
+        ]  
+    },
+    {
+        "anio": "2023",
+        "meses": [
+            {
+                "name": "Enero",
+                "productos": [
+                    {
+                        "id": "1",
+                        "name": "Producto 1",
+                        "precio": "1",
+                        "cantidad": "1"
+                    }
+                ]
+
+            },{
+                "name": "Febrero",
+                "productos": [
+                    {
+                        "id": "1",
+                        "name": "Producto 1",
+                        "precio": "1",
+                        "cantidad": "1"
+                    }
+                ]
+
+            }
+        ]  
+    },
+    {
+        "anio": "2024",
+        "meses": [
+            {
+                "name": "Enero",
+                "productos": [
+                    {
+                        "id": "1",
+                        "name": "Producto 1",
+                        "precio": "1",
+                        "cantidad": "1"
+                    }
+                ]
+
+            },{
+                "name": "Febrero",
+                "productos": [
+                    {
+                        "id": "1",
+                        "name": "Producto 1",
+                        "precio": "1",
+                        "cantidad": "1"
+                    }
+                ]
+
+            }
+        ]  
+    }
+]
+
+
 
 const JsonView = () => {
 
@@ -30,12 +148,83 @@ const JsonView = () => {
                     </Link>
                 </div>
 
+
+                <TableContainer>
+                    <Table variant="striped" colorScheme="">
+                        <TableCaption>Imperial to metric conversion factors</TableCaption>
+                        <Thead>
+                        <Tr>
+                            <Th>Año</Th>
+                            <Th>Mes</Th>
+                            <Th>Producto</Th>
+                            <Th>Precio</Th>
+                            <Th>Cantidad</Th>
+                        </Tr>
+                        </Thead>
+                        <Tbody>
+                            {
+                                json.map((item, index) => {
+                                    return (
+                                        <Tr key={index}>
+                                            <Td>{item.anio}</Td>
+                                            <Td colspan="4">
+                                                {
+                                                    item.meses.map((mes, index) => {
+                                                        return (
+                                                            <Table variant="unstyled">
+                                                                <Tbody>    
+                                                                    <Tr key={index}>
+                                                                        <Td>{mes.name}</Td>
+                                                                        <Td>
+                                                                            {
+                                                                                mes.productos.map((prod, index) => (
+                                                                                    <Table variant="unstyled">
+                                                                                        <Tbody>
+                                                                                            <Tr key={index}>
+                                                                                                <Td>{prod.name}</Td>
+                                                                                                <Td>{prod.precio}</Td>
+                                                                                                <Td>{prod.cantidad}</Td>
+                                                                                            </Tr>
+                                                                                        </Tbody>
+                                                                                    </Table>
+                                                                                ))
+                                                                            }
+                                                                        </Td>
+                                                                    </Tr>
+                                                                </Tbody>
+                                                            </Table>
+                                                        )
+                                                    })
+                                                }
+                                            </Td>
+                                        </Tr>
+                                    )
+                                } )
+                            }
+                        </Tbody>
+                        <Tfoot>
+                        <Tr>
+                            <Th>To convert</Th>
+                            <Th>into</Th>
+                            <Th isNumeric>multiply by</Th>
+                        </Tr>
+                        </Tfoot>
+                    </Table>
+                    </TableContainer>
+
             </Content>
+
+            
 
             <Footer />
 
         </div>
     )
 }
+
+
+
+
+
 
 export default JsonView
